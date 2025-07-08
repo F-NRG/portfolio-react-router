763f666851c165e351286a57a3cce1c6708b5952
Jonathan Verstraten <jverstraten@gmail.com>
2025-07-08 18:13:58 +0200

    test prerender config

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from '@react-router/dev/config';
 import { vercelPreset } from '@vercel/react-router/vite';
+import type { Params } from 'react-router';
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+
+  async prerender() {
+    return ['/'];
+  },
   presets: [vercelPreset()],
 } satisfies Config;
